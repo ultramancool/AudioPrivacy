319f20739f6ff227d2cdd424fbfba3000fff2d4e
ultra <ultramancool@gmail.com>
2017-02-22 12:22:26 -0500

    Upgrade to latest Android Studio, switch to new hook method inside systemui.

--- app/src/main/java/com/audio/privacy/AudioPrivacy.java
+++ app/src/main/java/com/audio/privacy/AudioPrivacy.java
@@ -1,13 +1,14 @@
 package com.audio.privacy;
 
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.media.MediaMetadata;
-import android.media.MediaMetadataRetriever;
-import de.robv.android.xposed.*;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
 
 
 public class AudioPrivacy implements IXposedHookZygoteInit, IXposedHookLoadPackage {
@@ -20,70 +21,19 @@ public class AudioPrivacy implements IXposedHookZygoteInit, IXposedHookLoadPacka
     public void initZygote(StartupParam startupParam) throws Throwable
     {
         XposedBridge.log("Loading AudioPrivacy...");
-        prefs = new XSharedPreferences(SettingsActivity.class.getPackage().getName());
-        prefs.makeWorldReadable();
-        updatePreferences(prefs);
 
     }
 
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-        findAndHookMethod(
-                android.media.MediaMetadata.Builder.class, "putBitmap", String.class, Bitmap.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        prefs = new XSharedPreferences(SettingsActivity.class.getPackage().getName());
-                        updatePreferences(prefs);
-                        if (enabled)
-                            param.args[1] = null;
-                    }
+        if (lpparam.packageName.equals("com.android.systemui")) {
+            Class psbClass = findClass("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader);
+            findAndHookMethod(psbClass, "updateMediaMetaData", boolean.class, new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                    return null;
                 }
-        );
-
-        findAndHookMethod(
-                android.media.RemoteControlClient.MetadataEditor.class,
-                "putBitmap", int.class, Bitmap.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param)
-                            throws Throwable {
-                        prefs = new XSharedPreferences(SettingsActivity.class.getPackage().getName());
-                        updatePreferences(prefs);
-                        if (enabled)
-                            param.args[1] = null;
-                    }
-                }
-        );
-
-        findAndHookMethod(
-                android.media.RemoteControlClient.MetadataEditor.class,
-                "putString", int.class, String.class, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param)
-                            throws Throwable {
-                        prefs = new XSharedPreferences(SettingsActivity.class.getPackage().getName());
-                        updatePreferences(prefs);
-                        if (hide_text) {
-                            int key = (Integer) param.args[0];
-                            switch (key) {
-                                case MediaMetadataRetriever.METADATA_KEY_ALBUM:
-                                case MediaMetadataRetriever.METADATA_KEY_ALBUMARTIST:
-                                case MediaMetadataRetriever.METADATA_KEY_AUTHOR:
-                                    param.args[1] = null;
-                                    break;
-                                case MediaMetadataRetriever.METADATA_KEY_TITLE:
-                                    param.args[1] = "Audio";
-                                    break;
-                            }
-                        }
-                    }
-                });
-    }
-
-    public static void updatePreferences(SharedPreferences prefs)
-    {
-        hide_text = prefs.getBoolean("hide_text_checkbox", true);
-        enabled = prefs.getBoolean("enable_checkbox", true);
+            });
+        }
     }
 }
